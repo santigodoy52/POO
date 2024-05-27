b5db563158dd60c0178eaeca5bf3d7c21a0455d4
santigodoy52 <110188938+santigodoy52@users.noreply.github.com>
2024-05-27 20:10:29 -0300

    Ejercicio13

---
+++ Ejercicio13/main.cpp
@@ -0,0 +1,35 @@
+#include <QApplication>
+#include <QWidget>
+#include <QHBoxLayout>
+#include <QSlider>
+#include <QSpinBox>
+
+int main(int argc, char** argv) {
+    QApplication a(argc, argv);
+
+    QWidget *ventana = new QWidget;  // Es la ventana padre (principal)
+    ventana->setWindowTitle("Volumen");
+    ventana->resize(300, 50);
+
+    QSpinBox *spinBox = new QSpinBox;
+    QSlider *slider = new QSlider(Qt::Horizontal);
+    spinBox->setRange(0, 100);
+    slider->setRange(0, 100);
+
+    QObject::connect(spinBox, SIGNAL(valueChanged(int)), slider, SLOT(setValue(int)));
+    QObject::connect(slider, SIGNAL(valueChanged(int)), spinBox, SLOT(setValue(int)));
+
+    spinBox->setValue(15);
+
+    QHBoxLayout *layout = new QHBoxLayout;
+    layout->addWidget(spinBox);
+    layout->addWidget(slider);
+    ventana->setLayout(layout);
+    ventana->setVisible(true);
+
+    QObject::connect(slider, &QSlider::valueChanged, [=](int value) {
+        ventana->setWindowTitle(QString::number(value));
+    });
+
+    return a.exec();
+}
